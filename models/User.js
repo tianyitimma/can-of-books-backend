022ee4943f69a70879f0d28527cc8921bc2b3d4c
mtygit <tim.ma0118@gmail.com>
2021-07-28 16:29:18 +0200

    setup mongoDB

---
+++ models/User.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true},
+  status: { type: String, required: true}
+})
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true},
+  books: [bookSchema]
+})
+
+module.export = mongoose.model('users', userSchema);
