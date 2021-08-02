4f389150e8c06ea36c7ef4ea4db28c1cf3b99d13
mtygit <tim.ma0118@gmail.com>
2021-08-02 18:00:52 +0200

    finished on the backend both lab13 and 14

--- models/User.js
+++ models/User.js
@@ -6,10 +6,11 @@ const bookSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true},
   status: { type: String, required: true}
+  //img: { type: String, required: true}
 })
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true},
+  email: { type: String, required: true, unique: true},
   books: [bookSchema]
 })
 
--- server.js
+++ server.js
@@ -3,90 +3,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
-const mongoose = require('mongoose');
-const UserBooks = require('./models/User.js');
-
 const app = express();
-app.use(cors());
-
 const PORT = process.env.PORT || 3001;
-const client = jwksClient({
-  jwksUri: 'https://tianyi.us.auth0.com/.well-known/jwks.json',
-});
+const mongoose = require('mongoose');
+const Book = require('./modules/book.js');
+const mongooseOptions = { useNewUrlParser: true, useUnifiedTopology: true }
 
-function getKey(header, callback){
-  client.getSigningKey(header.kid, function(err, key){
-    let sigingKey = key.publicKey || key.rsaPublicKey;
-    callback(null, sigingKey);
-  });
-}
+app.use(cors());
+app.use(express.json());
 
-app.get('/test', (req, res) => {
-
-  // TODO: 
-  // STEP 1: get the jwt from the headers
-  const token = req.headers.authorization.split(' ')[1];
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
-  jwt.verify(token, getKey, {}, function(err, user){
-    if(err) {
-      res.send('invalid token');
-    } else {
-      res.json({ 'token': token});
-    }
-  });
-
-})
+// connecting mongoose
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
-const mongooseOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+app.use('*', (req, res) => {
+  res.status(404),send('rout not found');
+})
 
-// connecting mongoose
-mongoose.connect('mongodb://localhost:27017/books', mongooseOptions);
+app.get('/books', Book.show);
+app.post('/books', Book.add);
+app.put('/books', Book.update);
+app.delete('/books', Book.delete);
 
-// creating user object and add books to database
-
-let tim = new UserBooks({
-  email: 'tim.ma0118@gmail.com',
-  books:[
-    {
-      name: 'Harry Potter',
-      description: 'Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling',
-      status: 'Finished reading it'
-    },
-    {
-      name: 'The Lord of the Rings',
-      description: 'The Lord of the Rings is an epic[1] high fantasy novel[a] by the English author and scholar J. R. R. Tolkien',
-      status: 'Finished reading it'
-    },
-    {
-      name: 'The Three-Body Problem',
-      description: 'The Three-Body Problem is a science fiction novel by the Chinese writer Liu Cixin',
-      status: 'Wanted to read'
-    }
-  ]
-});
-tim.save();
-
-app.get('/books', getBooks);
-
-function getBooks(req, res){
-  UserBooks.find({})
-    .then(books => {
-      res.json(books);
-    })
-}
-
-app.post('/books', createBooks);
-
-function createBooks(req, res) {
-  let user = new UserBooks(req.body);
-  user.save()
-    .then(user => {
-      res.json(user);
-    })
-}
